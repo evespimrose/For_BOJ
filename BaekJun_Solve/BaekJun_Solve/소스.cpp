57dc6317129ca90330f16606dc283853cd6a5d89
evespimrose <bigjht34@gmail.com>
2022-03-11 18:32:14 +0900

    2525번 - 오븐 시간 맞추기

--- BaekJun_Solve/BaekJun_Solve/소스.cpp
+++ BaekJun_Solve/BaekJun_Solve/소스.cpp
@@ -1,31 +1,26 @@
-// 2884
+// 2525
 #include <stdio.h>
 
 int main()
 {
 	int h = 0;
 	int m = 0;
+	int t = 0;
 	scanf("%d", &h);
 	scanf("%d", &m);
+	scanf("%d", &t);
 
-	if (h >= 0 && h <= 23 && m >= 0 && m <= 59)
+	if (h >= 0 && h <= 23 && m >= 0 && m <= 59 && t >= 0 && t <= 1000)
 	{
-		if (m >= 45)
+		h += t / 60;
+		m += t % 60;
+		if (m >= 60)
 		{
-			m = m - 45;
+			h++;
+			m %= 60;
 		}
-		else if (m < 45)
-		{
-			if (h > 0)
-			{
-				h = h - 1;
-			}
-			else
-			{
-				h = h + 23;
-			}
-			m = m + 15;
-		}
+		if (h >= 24) 
+			h %= 24;
 		printf("%d %d", h, m);
 	}
 }
