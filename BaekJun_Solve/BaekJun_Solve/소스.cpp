d1c9332be8590076e487f7c3da6ca81c1c31e039
evespimrose <bigjht34@gmail.com>
2022-07-13 19:44:05 +0900

    1110번 - 더하기 사이클

--- BaekJun_Solve/BaekJun_Solve/소스.cpp
+++ BaekJun_Solve/BaekJun_Solve/소스.cpp
@@ -1,4 +1,4 @@
-// 10952
+// 1110
 #include <iostream>
 
 using namespace std;
@@ -6,12 +6,22 @@ using namespace std;
 int main() {
 	int a;
 	int b;
-	
-	while(true)
+	int c;
+
+	cin >> a;
+	if (a < 10)
+		a *= 10;
+	int tmp = a;
+	int cycle = 0;
+
+	do
 	{
-		cin >> a >> b;
-		if (a == 0 && b == 0)
-			break;
-		cout << a + b << endl;
-	}
+		cycle++;
+		int i = tmp / 10;
+		int j = tmp % 10;
+		int k = i + j;
+		int l = k % 10;
+		tmp = j * 10 + l;
+	} while (tmp != a);
+	cout << cycle << endl;
 }
